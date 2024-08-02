59697e9948030b52e7cc3235007d2f9b9bd8ab90
vallzin <valmilson@gmail.com>
2024-08-02 17:22:03 -0300

    primeiro projeto

---
+++ Java-Como_Programar/java-como_programar/src/introducao_ao_java/BemVindo.java
@@ -0,0 +1,11 @@
+package introducao_ao_java;
+
+public class BemVindo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bem-vindo ao programa Java!");
+
+	}
+
+}
