45e94c6cd7b705efefe69c479b642f23999f9667
vallzin <valmilson@gmail.com>
2024-08-14 20:29:07 -0300

    printf

---
+++ Java-Como_Programar/introducaojava/src/Cap01/BemVindo4.java
@@ -0,0 +1,13 @@
+package Cap01;
+//terceira aplicação
+//imprimindo com formatação
+
+public class BemVindo4 {
+
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Bem-vindo a ", "programação com Java");
+        System.out.println("Especificador de formato para string:  %s ");
+        System.out.println("Especificador de formato para nova linha:  %n ");
+
+    }
+}
