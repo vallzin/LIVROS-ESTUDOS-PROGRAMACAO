e9ed212ea5bf93b3ee0abd91486feb99b8b31d41
vallzin <valmilson@gmail.com>
2024-08-14 20:02:28 -0300

    criado pacote Cap01

---
+++ Java-Como_Programar/introducaojava/src/Cap01/BemVindo.java
@@ -0,0 +1,12 @@
+package Cap01;
+//Primeira aplicação simples que exibe uma linha de texto
+
+public class BemVindo {
+
+    //método main para execução do aplicativo Java
+    public static void main(String[] args) {
+
+        System.out.println("Bem-vindo a programação Java");
+
+    }
+}
---
+++ Java-Como_Programar/introducaojava/src/Cap01/BemVindo2.java
@@ -0,0 +1,13 @@
+package Cap01;
+//Segunda aplicação, modificação da primeira aplicação
+//imprimindo com duas declarações de impressão diferentes
+
+public class BemVindo2{
+
+    //método main para execução do aplicativo Java
+    public static void main(String[] args) {
+
+        System.out.print("Bem-vindo a ");
+        System.out.println("programação com Java");
+    }
+}
