4a71c3be1f0a1c528accaa9a46db69a1e3aa9bb5
vallzin <valmilson@gmail.com>
2024-08-14 16:47:39 -0300

    segundo commit branch main

---
+++ Java-Como_Programar/introducaojava/src/BemVindo.java
@@ -0,0 +1,11 @@
+//imprimindo linha de texto com multiplas opções
+
+
+public class BemVindo {
+    //método main inicia a execução do aplicativo Java
+    public static void main(String[] args) {
+
+        System.out.print("Bem-vindo a ");
+        System.out.println("programação com Java");
+    }//fim método main
+}//fim da Classe Bemvindo
