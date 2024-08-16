e6bd388098083059e205e672481a9be8973eab56
vallzin <valmilson@gmail.com>
2024-08-16 13:28:12 -0300

    dados formatados

---
+++ java_como_programar/src/cap01/QuartoProgramaJava.java
@@ -0,0 +1,12 @@
+package cap01;
+//Texto com formatação
+//printf -> exibição de dados formatados
+
+public class QuartoProgramaJava {
+    public static void main(String[] args) {
+        System.out.println("\nQuartoProgramaJava.main()");
+        //o 'f' significa "formato"
+        System.out.printf("%s%n%s%n", "Bem-vindo a ", "programação com Java");
+        
+    }
+}
