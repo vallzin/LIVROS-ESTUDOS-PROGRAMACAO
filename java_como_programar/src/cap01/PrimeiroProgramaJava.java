baacce55a359a05d465a8b91ad95ae03e3d7b325
vallzin <valmilson@gmail.com>
2024-08-16 12:16:39 -0300

    deletado man, criado SegundoProgramaJav

--- java_como_programar/src/cap01/PrimeiroProgramaJava.java
+++ java_como_programar/src/cap01/PrimeiroProgramaJava.java
@@ -1,4 +1,8 @@
+package cap01;
+
+//exibindo texto em uma linha
 public class PrimeiroProgramaJava {
+
     public static void main(String[] args) {
         System.out.println("Bem-vindo a programação com Java");
     }
---
+++ java_como_programar/src/cap01/SegundoProgramaJava.java
@@ -0,0 +1,10 @@
+package cap01;
+//diferentes formas de exibir um texto
+public class SegundoProgramaJava {
+
+    public static void main(String[] args) {
+        System.out.print("Bem-vindo a ");
+        System.out.println("programação com Java");
+    }
+
+}
